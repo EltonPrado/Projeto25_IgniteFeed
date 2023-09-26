7e2db8b02075dee805065e7adff6a6e2bdf502ed
elton <eltonm.prado@gmail.com>
2023-09-26 08:12:47 -0300

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1463130456064-77fda7f96d6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" 
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/EltonPrado.png" />
+
+        <strong>Elton Prado</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
